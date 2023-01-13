e6d91a647d80dd658bd5f0e80e3b7d70a15e1d8d
apple <apple@appledeMacBook-Pro.local>
2023-01-13 10:36:25 +0800

    test if the change apply to devops

--- src/pages/Navibar.jsx
+++ src/pages/Navibar.jsx
@@ -90,7 +90,7 @@ const Navibar = () => {
             <p>
               <span className="text-gray-400 text-14">Hi,</span>{' '}
               <span className="text-gray-400 font-bold ml-1 text-14">
-                Shaun
+                Shuan
               </span>
             </p>
             <MdKeyboardArrowDown className="text-gray-400 text-14" />
